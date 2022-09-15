7122d0835372f4a865f00964cfa3013ab6ee9a0a
muhammad <ha4ukaev.m@gmail.com>
2022-09-15 19:20:48 +0300

    react-router-installed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from "./logo.svg";
 import "./scss/app.scss";
 import Categories from "./Components/Categories";
 import Header from "./Components/Header";
 import Sort from "./Components/Sort";
-import Pizzablock from "./Components/Pizzablock";
+import Pizzablock from "./Components/PizzaBlock/Pizzablock";
 import React from "react";
+import { Skeleton } from "./Components/PizzaBlock/Skeleton";
 function App() {
   const [PizzaItems, setPizzaItems] = React.useState([]);
+  const [IsLoading, setIsLoading] = React.useState(true);
   React.useEffect(() => {
     fetch("https://6322c84ca624bced307e6cf0.mockapi.io/Pizzas").then((res) =>
       res.json()
     ).then((arr)=>{
       setPizzaItems(arr);
+      setIsLoading(false);
     })
   },[]);
 
@@ -26,9 +28,10 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {PizzaItems.map((pzsitem) => (
-              <Pizzablock key={pzsitem.id} {...pzsitem} />
-            ))}
+       
+            {
+              IsLoading? [...new Array(6)].map((_,index)=>(<Skeleton key={index}/>)):PizzaItems.map((items,index)=><Pizzablock key={index} {...items}/>)
+            }
           </div>
         </div>
       </div>
---
+++ src/Components/PizzaBlock/Skeleton.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+export const Skeleton = () => (
+  <ContentLoader
+    className="pizza-block"
+    speed={2}
+    width={280}
+    height={500}
+    viewBox="0 0 280 500"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb">
+    <circle cx="134" cy="136" r="125" />
+    <rect x="0" y="279" rx="10" ry="10" width="280" height="23" />
+    <rect x="0" y="315" rx="10" ry="10" width="280" height="87" />
+    <rect x="0" y="430" rx="10" ry="10" width="95" height="25" />
+    <rect x="125" y="425" rx="24" ry="24" width="152" height="45" />
+  </ContentLoader>
+);
